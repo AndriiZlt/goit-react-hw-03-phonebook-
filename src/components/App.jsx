04684de673eba7b8364a8803f297775b6d1193bd
AndriiZlt <andreyzlot@gmail.com>
2023-05-31 12:37:41 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,27 @@ import ContactList from './Contacts/Contacts';
 class App extends React.Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({
+      contacts: savedContacts,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('changed');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addingNewContact = e => {
     const { name, number } = e.currentTarget;
@@ -25,7 +39,6 @@ class App extends React.Component {
     }
 
     if (!isTaken) {
-      console.log('yes');
       const contactId = shortid.generate();
       const newContact = {
         id: contactId,
